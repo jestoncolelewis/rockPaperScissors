3f30a19f7a1af697464741bf13fd1f076c6dbc78
Jeston Lewis <jeston.lewis@protonmail.com>
2022-12-27 10:59:56 -0700

    added nodes for point results and round results

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let choices = ['rock','paper','scissors'];
+let choices = ['rock', 'paper', 'scissors'];
 let playerWins = 0;
 let computerWins = 0;
 
@@ -7,17 +7,18 @@ function playerChoice(playerSelection) {
         console.log('You played Rock');
         return choices[0];
     } else if (playerSelection === choices[1]) {
-        console.log('You played Paper'); 
+        console.log('You played Paper');
         return choices[1];
     } else if (playerSelection === choices[2]) {
         console.log('You played Scissors');
         return choices[2];
     } else {
         playerChoice();
-    }};
+    }
+};
 
 function computerChoice() {
-    let computerSelection = Math.floor(Math.random()*choices.length)
+    let computerSelection = Math.floor(Math.random() * choices.length)
     if (computerSelection === 0) {
         console.log('Computer played Rock');
         return choices[0];
@@ -27,34 +28,67 @@ function computerChoice() {
     } else {
         console.log('Computer played Scissors');
         return choices[2];
-    }};
+    }
+};
+
+// result nodes
+const results = document.querySelector('#results');
+const winner = document.createElement('h3');
+function removeResults() {
+    results.removeChild(winner);
+}
 
 function round(playerChoice, computerChoice) {
     switch (true) {
         case playerChoice === choices[0] && computerChoice === choices[2]:
         case playerChoice === choices[2] && computerChoice === choices[1]:
         case playerChoice === choices[1] && computerChoice === choices[0]:
-            console.log('You win');
+            winner.textContent = 'You win this round';
+            results.appendChild(winner);
+            setTimeout(
+                removeResults,
+                1500
+            );
             return playerWins++
         case computerChoice === choices[0] && playerChoice === choices[2]:
         case computerChoice === choices[2] && playerChoice === choices[1]:
         case computerChoice === choices[1] && playerChoice === choices[0]:
-            console.log('Computer wins');
+            winner.textContent = 'Computer wins this round';
+            results.appendChild(winner);
+            setTimeout(
+                removeResults,
+                1500
+            );
             return computerWins++
         case computerChoice === playerChoice:
-            console.log('It\'s a tie!');
+            winner.textContent = 'It\'s a tie';
+            results.appendChild(winner);
+            setTimeout(
+                removeResults,
+                1500
+            );
             break
-        }};
+    }
+};
 
+// player win count node
+const playerw = document.querySelector('#playerw');
+const ppoints = document.createElement('p');
 
-        
+// computer win count node
+const compw = document.querySelector('#compw');
+const cpoints = document.createElement('p');
+
 // playerChoice using buttons
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        playerChoice(button.id);
-        computerChoice();
-        round(playerChoice, computerChoice);
+        round(playerChoice(button.id), computerChoice());
+
+        ppoints.textContent = playerWins;
+        playerw.appendChild(ppoints);
+        cpoints.textContent = computerWins;
+        compw.appendChild(cpoints);
     });
 });
